bbc9d0762ef1975f65b2aef54d6c201af1a4a357
shaojun <137772740@qq.com>
2022-11-05 23:16:42 +0800

    添加ignore文件

--- pattern-demo/src/main/java/org/example/observer/WebObserver.java
+++ pattern-demo/src/main/java/org/example/observer/WebObserver.java
@@ -6,6 +6,11 @@ public class WebObserver implements Observer{
         subject.registerObserver(this);
     }
 
+    /**
+     * 做某些事情
+     *
+     * @param msg 消息
+     */
     @Override
     public void update(String msg) {
         System.out.println("Received msg from subject : " + msg + " , do nothing..");
